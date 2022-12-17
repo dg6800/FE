0596d8a1cd2da4f0c83e9fe5560c81be1d49c1c4
Hyeji Kang <owlgpwl94@gmail.com>
2022-12-17 02:53:50 -0800

    메인페이지 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Main = () => {
     return (
-        <div>
-            메인페이지
-        </div>
+        <IntroDiv>
+            4조 프로젝트 소개 공간입니다.
+        </IntroDiv>
     );
 };
 
 export default Main;
+
+const IntroDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
