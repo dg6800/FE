da71ec82be93148d48d77d6099f08c03e3c2fb2b
Hyeji Kang <owlgpwl94@gmail.com>
2022-12-17 01:05:50 -0800

    로그인페이지 링크

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const menuList = ['정보방', '롤링페이퍼']
+  
+    const navigate = useNavigate();
+  
+  
+    const goToLogin = () => {
+       navigate('/login');
+    }
+  
   return (
     <div>
-      <LoginBox>
+      <LoginBox onClick={goToLogin}>
         <div><FontAwesomeIcon icon={faUser} /></div>
         <div>로그인</div>
       </LoginBox>
@@ -27,6 +36,7 @@ export default Navbar;
 const LoginBox = styled.div`
 display: flex;
 justify-content: flex-end;
+cursor: pointer;
 `
 
 const NavBox = styled.div`
