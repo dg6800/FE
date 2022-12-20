42e66bf0633518ced7b8ec6ad22ec5c1f0c92b08
SeungwooYang <dg6800000@gmail.com>
2022-12-19 17:10:42 -0800

    자잘한거
    자잘한거

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,15 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from '../pages/Main';
-import LogIn from '../pages/LogIn';
-import SignUp from '../pages/SignUp';
-import Board from '../pages/Boards';
-import Navbar from '../components/layout/Navbar';
-import BoardCreate from '../pages/BoardCreate'
-import Rolling from '../pages/Rolling';
-import BoardDetail from '../pages/BoardDetail';
-
-
+import Main from "../pages/Main";
+import LogIn from "../pages/LogIn";
+import SignUp from "../pages/SignUp";
+import Board from "../pages/Boards";
+import Navbar from "../components/layout/Navbar";
+import BoardCreate from "../pages/BoardCreate";
+import Rolling from "../pages/Rolling";
+import BoardDetail from "../pages/BoardDetail";
 
 //1. 메인페이지, 로그인페이지, 정보공유 메인페이지,(o)
 // 네이게이션 바 만들거임.(o)
@@ -18,32 +16,29 @@ import BoardDetail from '../pages/BoardDetail';
 //4. 로그인이 안되어있을경우 로그인 페이지가 나오게 된다.
 
 const Router = () => {
-
-
   //로그인을 한user인지 아닌지 구별하기 위한 코드
-    const[authenticate, setAuthenticate]= useState(false) // false: 로그인안됨. true:로그인됨
-    useEffect(()=>{
-        console.log("aaa",authenticate)
-    },[authenticate])
+  const [authenticate, setAuthenticate] = useState(false); // false: 로그인안됨. true:로그인됨
+  useEffect(() => {
+    console.log("aaa", authenticate);
+  }, [authenticate]);
 
-    return (
-        <BrowserRouter>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/api/users/login" element={<LogIn setAuthenticate={setAuthenticate}/>} />
-                <Route path="/api/users/signup" element={<SignUp />} />
-                <Route path="/api/boards" element={<BoardCreate />} />
-                <Route path="/api/boards/main" element={<Board />} />
-                <Route path="/api/boards/:id" element={<BoardDetail/>} />
-                <Route path="/api/boards/:id" element={<BoardDetail/>} />
-                <Route path="/api/rollingpapers" element={<Rolling />} />
-                
-                
-                
-            </Routes>
-        </BrowserRouter> 
-    );
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/api/users/login"
+          element={<LogIn setAuthenticate={setAuthenticate} />}
+        />
+        <Route path="/api/users/signup" element={<SignUp />} />
+        <Route path="/api/boards" element={<BoardCreate />} />
+        <Route path="/api/boards/main" element={<Board />} />
+        <Route path="/api/boards/:id" element={<BoardDetail />} />
+        <Route path="/api/rollingpapers" element={<Rolling />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
